a8517bdf0a71d870f9c4bb3ea0a958ad595143e1
victorrlo <42052096+victorrlo@users.noreply.github.com>
2024-11-21 22:10:49 -0300

    camera collision working properly

--- Assets/Scripts/Player/PlayerCamera.cs
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -14,17 +14,22 @@ namespace Brastor
 
         [Header("Camera Settings")]
         private float cameraSmoothSpeed = 1; //the bigger, the longer for the camera to reach its position during movement
-        [SerializeField] float leftAndRightRotationSpeed = 220;
-        [SerializeField] float upAndDownRotationSpeed = 220;
-        [SerializeField] float minimumPivot = -30; //lowest point player can look down
-        [SerializeField] float maximumPivot = 60; //highest point player can look up
+        [SerializeField] float _leftAndRightRotationSpeed = 220;
+        [SerializeField] float _upAndDownRotationSpeed = 220;
+        [SerializeField] float _minimumPivot = -30; //lowest point player can look down
+        [SerializeField] float _maximumPivot = 60; //highest point player can look up
+        [SerializeField] float _cameraCollisionRadius = 0.2f;
 
 
 
         [Header("Camera Values")]
-        private Vector3 cameraVelocity;
-        [SerializeField] float leftAndRightLookAngle;
-        [SerializeField] float upAndDownLookAngle;
+        private Vector3 _cameraVelocity;
+        private Vector3 _cameraObjectPosition; // used for camera colisions (moves camera to this position when colliding)
+        [SerializeField] float _leftAndRightLookAngle;
+        [SerializeField] float _upAndDownLookAngle;
+        private float _cameraZPosition;
+        private float _targetCameraZPosition;
+        [SerializeField] LayerMask _collideWithLayers;
 
         private void Awake()
         {
@@ -42,6 +47,7 @@ namespace Brastor
         private void Start()
         {
             DontDestroyOnLoad(gameObject);
+            _cameraZPosition = _camera.transform.localPosition.z;
         }
 
         public void HandleAllCameraActions()
@@ -52,16 +58,16 @@ namespace Brastor
 
                 //rotate around the player
                 HandleRotation();
-                
-                //collide with objects
 
+                //collide with objects
+                HandleColisions();
             }
 
         }
 
         private void HandleFollowTarget()
         {
-            Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, _player.transform.position, ref cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
+            Vector3 targetCameraPosition = Vector3.SmoothDamp(transform.position, _player.transform.position, ref _cameraVelocity, cameraSmoothSpeed * Time.deltaTime);
             transform.position = targetCameraPosition;
         }
 
@@ -72,26 +78,48 @@ namespace Brastor
 
             //normal rotations
             //rotate left and right based on horizontal movement on the right joystick
-            leftAndRightLookAngle += (InputManager._instance._cameraHorizontalInput * leftAndRightRotationSpeed) * Time.deltaTime;
+            _leftAndRightLookAngle += (InputManager._instance._cameraHorizontalInput * _leftAndRightRotationSpeed) * Time.deltaTime;
             //rotate up and down based on vertical movement on the right joystick
-            upAndDownLookAngle -= (InputManager._instance._cameraVerticalInput *upAndDownRotationSpeed) * Time.deltaTime;
+            _upAndDownLookAngle -= (InputManager._instance._cameraVerticalInput *_upAndDownRotationSpeed) * Time.deltaTime;
             //clamp up the up and down look angle between a min and max value
-            upAndDownLookAngle = Mathf.Clamp(upAndDownLookAngle, minimumPivot, maximumPivot);
+            _upAndDownLookAngle = Mathf.Clamp(_upAndDownLookAngle, _minimumPivot, _maximumPivot);
 
 
             Vector3 cameraRotation = Vector3.zero;
             Quaternion targetRotation;
 
             //rotates this gameobject left and right
-            cameraRotation.y = leftAndRightLookAngle;
+            cameraRotation.y = _leftAndRightLookAngle;
             targetRotation = Quaternion.Euler(cameraRotation);
             transform.rotation = targetRotation;
 
             //rotates this gameobject up and down
-            cameraRotation.x = upAndDownLookAngle;
+            cameraRotation.x = _upAndDownLookAngle;
             targetRotation = Quaternion.Euler(cameraRotation);
             _cameraPivotTransform.localRotation = targetRotation;
 
+        }
+
+        private void HandleColisions()
+        {
+            _targetCameraZPosition = _cameraZPosition;
+            RaycastHit hit;
+            Vector3 direction = _camera.transform.position - _cameraPivotTransform.position;
+            direction.Normalize();
+
+            if (Physics.SphereCast(_cameraPivotTransform.position, _cameraCollisionRadius, direction, out hit, Mathf.Abs(_targetCameraZPosition), _collideWithLayers))
+            {
+                float distanceFromHitObject = Vector3.Distance(_cameraPivotTransform.position, hit.point);
+                _targetCameraZPosition = -(distanceFromHitObject - _cameraCollisionRadius);
+            }
+
+            if (Mathf.Abs(_targetCameraZPosition) < _cameraCollisionRadius)
+            {
+                _targetCameraZPosition = -_cameraCollisionRadius;
+            }
+
+            _cameraObjectPosition.z = Mathf.Lerp(_camera.transform.localPosition.z, _targetCameraZPosition, 0.2f);
+            _camera.transform.localPosition = _cameraObjectPosition;
         }
     }
 }
